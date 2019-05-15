ba28300006e5c1e6732984a252df3b084018671d
macshakshober <macshakshober@gmail.com>
2019-05-15 15:14:31 -0400

    Enhanced Comments

--- HttpHeaderInterpreter.c
+++ HttpHeaderInterpreter.c
@@ -10,6 +10,7 @@
 "sdfkjsdnbfkjbsf"
 */
 
+// Builds linked list based on headerString
 struct httpheader* getHttpHeaderStruct(char* headerString)
 {
 	struct httpheader* httpHeaderPtr = malloc(sizeof(struct httpheader));
@@ -18,25 +19,30 @@ struct httpheader* getHttpHeaderStruct(char* headerString)
 	// Get the First Line
 	for (i = 0; i < strlen(headerString); ++i)
 	{
+		// '\n' functions as a seperator
 		if (headerString[i] == '\n')
 		{
 			httpHeaderPtr->firstline = malloc(i * sizeof(char));
 			strncpy(httpHeaderPtr->firstline, headerString, i);
+			// Set begin to the first index of the substring that will be passed to the helper method
 			begin = i + 1;
 			break;
 		}
 	}
 
 	// Get the Data
+	// The data is located at the end of the headerString
 	for (i = strlen(headerString) - 1; i >= 0; --i)
 	{
+		// '\n' functions as a seperator
 		if (headerString[i] == '\n')
 		{
 			++i;
 			// strlen(headerString) - i = length of the data
 			httpHeaderPtr->data = malloc((strlen(headerString) - i) * sizeof(char));
-			// header + i = address of where data starts, strlen(headerString) - i = length of the data
+			// header + i = index of where data starts, strlen(headerString) - i = length of the data
 			strncpy(httpHeaderPtr->data, headerString + i, strlen(headerString) - i);
+			// Set length to be the length of the substring that will be passed to the helper method
 			length = i - begin - 1;
 			break;
 		}
@@ -46,12 +52,14 @@ struct httpheader* getHttpHeaderStruct(char* headerString)
 	char* headerSubString = malloc(length * sizeof(char));
 	strncpy(headerSubString, headerString + begin, length);
 
+	// Set the next element of the linked list to be the httpheaderitem of the next line
 	httpHeaderPtr->item = getHttpItemStruct(headerSubString);
 	free(headerSubString);
 
 	return httpHeaderPtr;
 }
 
+// Helper method to recursively build linked list
 struct httpheaderitem* getHttpItemStruct(char* headerString)
 {
 	struct httpheaderitem* httpItemPtr = malloc(sizeof(struct httpheaderitem));
@@ -59,23 +67,28 @@ struct httpheaderitem* getHttpItemStruct(char* headerString)
 	int i, start;
 
 	// Get the Key
+	// Example: "Content-Length: 15\n" Key = Content-Length
 	for (i = 0; i < strlen(headerString); ++i)
 	{
+		// ':' functions as a seperator
 		if (headerString[i] == ':')
 		{
 			httpItemPtr->key = malloc(i * sizeof(char));
 			strncpy(httpItemPtr->key, headerString, i);
+			// Set start to be the start index of the val section of the string
 			start = i + 2;
 			break;
 		}
 	}
 
 	// Get the Value
+	// Example: "Content-Length: 15\n" Value = 15
 	for (i++; i < strlen(headerString); ++i)
 	{
+		// '\n' functions as a seperator
 		if (headerString[i] == '\n')
 		{
-			// length of val = i -start
+			// length of val = i - start
 			httpItemPtr->val = malloc((i - start) * sizeof(char));
 			// headerString + start = address of where val starts, i - start = length of the val
 			strncpy(httpItemPtr->val, headerString + start, (i - start));
@@ -88,14 +101,17 @@ struct httpheaderitem* getHttpItemStruct(char* headerString)
 	char* headerSubString = malloc(length * sizeof(char));
 	strncpy(headerSubString, headerString + i + 1, length);
 
+	// if all lines have been processed and the headerSubString is empty then return the last element of the list
 	if (strlen(headerSubString) == 0)
 	{
 		httpItemPtr->next = NULL;
 		free(headerSubString);
 		return httpItemPtr;
 	}
+	// else there are more lines to process
 	else
 	{
+		// set httpItemPtr->next to the result of calling this function again with the reduced headerSubString
 		httpItemPtr->next = getHttpItemStruct(headerSubString);
 		free(headerSubString);
 		return httpItemPtr;
@@ -112,6 +128,7 @@ struct httpheaderitem* getHttpItemStruct(char* headerString)
 "sdfkjsdnbfkjbsf"
 */
 
+// Constructs the Http Header String from the linked list
 void getHttpHeaderString(char* headerString, struct httpheader* httpHeaderPtr)
 {
 	// Add first line
@@ -130,7 +147,6 @@ void getHttpHeaderString(char* headerString, struct httpheader* httpHeaderPtr)
 	// Add data
 	strcat(headerString, "\n");
 	strcat(headerString, httpHeaderPtr->data);
-	strcat(headerString, "\0");
 }
 
 void freeHeader(struct httpheader* httpHeaderPtr)
