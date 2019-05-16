92b69408bcf9046893b0b06595e140c3ba0a7411
Andreweweith <andreweweith@gmail.com>
2019-05-16 03:53:46 -0400

    Webpage Comments Update

--- wrcserver_js.js
+++ wrcserver_js.js
@@ -1,6 +1,6 @@
 var stdout;
 var stderr;
-
+// On window load, initialize variables with the HTML elements
 window.onload = function() {
     var formRun = document.getElementById("run");
     var formCommand = document.getElementById("command");
@@ -10,8 +10,11 @@ window.onload = function() {
     stdout = document.getElementById("stdout");
     stderr = document.getElementById("stderr");
 
+    // When button is clicked, disable to prevent further input,
+    // Then populate the commandBox with the input command,
+    // open/handle then send request. Re-enable button when finished.
     formRun.onclick = function run() {
-	formRun.disabled= true;
+        formRun.disabled= true;
         var commandText = document.getElementById("command").value;
 
         formCommand.value = "";
@@ -23,19 +26,20 @@ window.onload = function() {
         request.onload = handleResponse;
 
         request.send(null);
+        formRun.disabled= false;
     }
 }
 
+// Function to handle response, checks for correct status then process response text
 function handleResponse() {
     if (this.status == 200 && this.responseText != null) {
 
         var response = JSON.parse(this.responseText);
 
-        stdout.value += response.stdout;
-        stderr.value += response.stderr;
+        stdout.value += response.stdout + "\n";
+        stderr.value += response.stderr + "\n";
     }
     else if (this.status == 404) {
         console.log("404 - Error has occurred");
     }
-    document.getElementById("run").disabled= false;
 }
